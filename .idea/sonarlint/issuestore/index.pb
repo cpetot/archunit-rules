589de045f78395ee75dabbf2d5b53fd99ffbfdff
Claude Petot <claude.petot@viseo.com>
2022-08-17 09:48:15 +0200

    Adding Javadoc

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+7
+LICENSE,0\3\0398ccd0f49298b10a3d76a47800d2ebecd49859
+ç
+]src/test/java/io/github/cpetot/archunit/jpa/TestEntityWithImplicitPublicEmptyConstructor.java,9\d\9d1c7d8c2b5df2ec271c5a73d41b8f967dbb7e15
+m
+=src/test/java/io/github/cpetot/archunit/date/DateExample.java,6\d\6dfb3cf54bb587b58c4bfb35898129502d6b9635
+Ö
+Usrc/test/java/io/github/cpetot/archunit/jpa/TestEntityWithPublicArgedConstructor.java,f\a\fa01cea73f6a0251e4e1bce978730ca540f4aebf
+Ü
+Vsrc/test/java/io/github/cpetot/archunit/jpa/TestEntityWithPrivateEmptyConstructor.java,f\1\f174c10b4a965c317b9f128e87b6496b8ae2e949
+r
+Bsrc/test/java/io/github/cpetot/archunit/date/LocalDateExample.java,2\f\2f6f7f79eb0711832663457f63ecb727a6e0ed0d
--- src/main/java/io/github/cpetot/archunit/StandardCodingRules.java
+++ src/main/java/io/github/cpetot/archunit/StandardCodingRules.java
@@ -16,11 +16,19 @@ import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
 
+/**
+ * StandardCodingRules provides a set of general {@link ArchCondition ArchConditions}
+ * and {@link ArchRule ArchRules} for coding that might be useful in various projects
+ * but are not present in {@link com.tngtech.archunit.library.GeneralCodingRules GeneralCodingRules}.
+ */
 public final class StandardCodingRules {
 
 	private StandardCodingRules() {
 	}
 
+	/**
+	 * A condition that checks that the given class use JUnit 4.
+	 */
 	@PublicAPI(usage = ACCESS)
 	public static final ArchCondition<JavaClass> USE_JUNIT_4 = useJunit4();
 
@@ -29,12 +37,24 @@ public final class StandardCodingRules {
 			.as("use JUnit 4");
 	}
 
+	/**
+	 * A rule that checks that the given class does not use JUnit 4.
+	 *
+	 * <p>
+	 * Junit 5 is recommanded, but since some test annotations are similar, it may be easy to use the old version.
+	 * </p>
+	 *
+	 * @see StandardCodingRules#USE_JUNIT_4
+	 */
 	@PublicAPI(usage = ACCESS)
 	public static final ArchRule NO_CLASSES_SHOULD_USE_JUNIT_4 =
 		noClasses()
 			.should(USE_JUNIT_4)
 			.because("Use JUnit 5 instead");
 
+	/**
+	 * A condition that checks if the given class use {@link Date}
+	 */
 	@PublicAPI(usage = ACCESS)
 	public static final ArchCondition<JavaClass> USE_JAVA_UTIL_DATE = useJavaUtilDate();
 
@@ -43,11 +63,57 @@ public final class StandardCodingRules {
 			.as("use java.util.Date");
 	}
 
+	/**
+	 * A rule that checks that all the given classes do not use {@link Date}
+	 *
+	 * <p>
+	 * Since Java 8, all classes in {@code java.time.*} are recommended to deal with date and time.
+	 * </p>
+	 *
+	 * @see StandardCodingRules#USE_JAVA_UTIL_DATE
+	 */
+	@PublicAPI(usage = ACCESS)
 	public static final ArchRule NO_CLASSES_SHOULD_USE_JAVA_UTIL_DATE =
 		noClasses()
 			.should(USE_JAVA_UTIL_DATE)
 			.because("Use dates API in java.time instead");
 
+	/**
+	 * A condition that checks if the Java class has a public no-args constructor
+	 * <p>
+	 * Matching examples :
+	 * <pre>{@code
+	 * public class MyClass {
+	 *   // Fields
+	 *   public MyClass() {}
+	 * }
+	 *
+	 * public class MyClass {
+	 *   // Implicit public constructor
+	 * }
+	 *
+	 * }
+	 * </pre>
+	 * </p>
+	 *
+	 * <p>
+	 * Not marching examples :
+	 * <pre>{@code
+	 * public class MyClass {
+	 *   // Fields
+	 *   public MyClass(String description) {
+	 *     this.description = description
+	 *   }
+	 * }
+	 *
+	 * public class MyClass {
+	 *   // Fields
+	 *   private MyClass() {}
+	 * }
+	 * }
+	 * </pre>
+	 * </p>
+	 */
 	@PublicAPI(usage = ACCESS)
 	public static final ArchCondition<JavaClass> HAS_A_PUBLIC_EMPTY_CONSTRUCTOR = hasAPublicEmptyConstructor();
 
